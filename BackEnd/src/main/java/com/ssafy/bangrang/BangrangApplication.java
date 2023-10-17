db6af0d4462d7c6417e5eec367bcba17f50138b6
aldms8960 <45choi22@gmail.com>
2023-10-17 13:51:42 +0900

    Init: init springboot project java version: 17 - spring boot version: 3.1.4 - database: mysql

---
+++ BackEnd/src/main/java/com/ssafy/bangrang/BangrangApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.bangrang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BangrangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BangrangApplication.class, args);
+    }
+
+}
---
+++ BackEnd/src/test/java/com/ssafy/bangrang/BangrangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.bangrang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BangrangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
