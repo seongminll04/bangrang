68ce7ef55a822569c96c9d19a7ade588c5acf2e2
DobroL33 <dobrolee26@gmail.com>
2023-10-30 16:31:25 +0900

    feat/fe_web/소개페이지 1,2 배경

---
+++ FrontEnd_Web/src/pages/intropages/SecondPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./SecondPage.module.css";
+
+const SecondPage: React.FC = () => {
+  return (
+    <div>
+      <div className={styles.Background}></div>
+    </div>
+  );
+};
+
+export default SecondPage;
