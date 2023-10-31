6c7ea7887cfd0fbb0d2d8036c91b530fa625286c
aldms8960 <45choi22@gmail.com>
2023-10-31 16:04:17 +0900

    Feat: 일대일 문의 등록 완료

--- BackEnd/src/main/java/com/ssafy/bangrang/domain/inquiry/api/InquiryApi.java
+++ BackEnd/src/main/java/com/ssafy/bangrang/domain/inquiry/api/InquiryApi.java
@@ -2,17 +2,16 @@ package com.ssafy.bangrang.domain.inquiry.api;
 
 
 import com.ssafy.bangrang.domain.event.api.response.GetEventAllResponseDto;
+import com.ssafy.bangrang.domain.inquiry.api.request.AddInquiryRequestDto;
 import com.ssafy.bangrang.domain.inquiry.api.response.GetInquiryAllResponseDto;
+import com.ssafy.bangrang.domain.inquiry.service.InquiryService;
 import com.ssafy.bangrang.domain.member.entity.AppMember;
 import com.ssafy.bangrang.domain.member.service.AppMemberService;
 import com.ssafy.bangrang.global.security.jwt.JwtService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -26,6 +25,7 @@ public class InquiryApi {
 
     private final JwtService jwtService;
     private final AppMemberService appMemberService;
+    private final InquiryService inquiryService;
 
     // 사용자 일대일 문의 리스트
     @GetMapping("/list")
@@ -39,5 +39,19 @@ public class InquiryApi {
         log.info("[사용자 일대일 문의 리스트 요청 끝]");
 
         return ResponseEntity.ok().body(inquiryList);
+    }
+
+    // 사용자 일대일 문의 등록
+    @PostMapping("/resist")
+    public ResponseEntity addInquiry(@RequestHeader("Authorization") String accessToken, @RequestBody AddInquiryRequestDto addInquiryRequestDto){
+
+        log.info("[사용자 일대일 문의 등록 요청 시작]", LocalDateTime.now());
+
+        String id = jwtService.extractEmail(accessToken).orElseThrow();
+        inquiryService.saveInquiry(id, addInquiryRequestDto);
+
+        log.info("[사용자 일대일 문의 등록 요청 끝]");
+
+        return ResponseEntity.ok().build();
     }
 }
--- BackEnd/src/main/java/com/ssafy/bangrang/domain/inquiry/service/InquiryService.java
+++ BackEnd/src/main/java/com/ssafy/bangrang/domain/inquiry/service/InquiryService.java
@@ -1,4 +1,8 @@
 package com.ssafy.bangrang.domain.inquiry.service;
 
+import com.ssafy.bangrang.domain.inquiry.api.request.AddInquiryRequestDto;
+import com.ssafy.bangrang.domain.inquiry.entity.Inquiry;
+
 public interface InquiryService {
+    Inquiry saveInquiry(String id, AddInquiryRequestDto addInquiryRequestDto);
 }
