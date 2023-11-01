fe7d7a2a7c7c321e876f086d8741d96d9a0ac85b
이성민 <lrune2el@naver.com>
2023-11-01 10:33:10 +0900

    fix : import error

--- BackEnd/src/main/java/com/ssafy/bangrang/domain/member/api/AppMemberApi.java
+++ BackEnd/src/main/java/com/ssafy/bangrang/domain/member/api/AppMemberApi.java
@@ -12,10 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
@@ -26,6 +23,8 @@ import java.time.LocalDateTime;
 public class AppMemberApi {
 
     private final AppMemberService appMemberService;
+
+    private final JwtService jwtService;
 
     @ApiOperation(value = "닉네임 중복 확인")
     @GetMapping("/nicknameCheck")
