75591cd39c34a48a5af8e39c1a6b616e9fe2eea6
DobroL33 <dobrolee26@gmail.com>
2023-11-01 15:04:58 +0900

    [S09P31B109-223] Feat/Fe-Web/세번째 페이지 및 다른 페이지 css retouching

--- FrontEnd_Web/src/pages/introhome.tsx
+++ FrontEnd_Web/src/pages/introhome.tsx
@@ -1,13 +1,15 @@
 import React from "react";
+import MainPage from "./intropages/MainPage";
 import FirstPage from "./intropages/FirstPage";
 import SecondPage from "./intropages/SecondPage";
 import ThirdPage from "./intropages/ThirdPage";
-
+import styles from "./introhome.module.css";
 const IntroHome: React.FC = () => {
   return (
-    <div>
+    <div className={styles.Page}>
+      <MainPage></MainPage>
       <FirstPage></FirstPage>
-      {/* <SecondPage></SecondPage> */}
+      <SecondPage></SecondPage>
       <ThirdPage></ThirdPage>
     </div>
   );
--- FrontEnd_Web/src/pages/intropages/FirstPage.tsx
+++ FrontEnd_Web/src/pages/intropages/FirstPage.tsx
@@ -1,20 +1,8 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./FirstPage.module.css";
-import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const FirstPage: React.FC = () => {
-  return (
-    <div className={styles.Page}>
-      <div className={styles.btnContainer}>
-        <Link className={` ${styles.down}`} to="/login">
-          관리자 페이지
-        </Link>
-        <Link className={`${styles.down}`} to="https://play.google.com/store">
-          다운로드
-        </Link>
-      </div>
-    </div>
-  );
+  return <div className={styles.Page}></div>;
 };
 
 export default FirstPage;
