e647a2f35d72dbcc705e215740931be766646225
이성민 <lrune2el@naver.com>
2023-11-03 12:51:20 +0900

    feat : token 재발급 api

--- BackEnd/src/main/java/com/ssafy/bangrang/global/security/jwt/JwtAuthenticationFilter.java
+++ BackEnd/src/main/java/com/ssafy/bangrang/global/security/jwt/JwtAuthenticationFilter.java
@@ -60,18 +60,19 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             return;
         }
 
-        // 일반 로그인 아니면 계속해서 진행
+        // 검증이 필요한 요청은 진행
 
-        // 요청 헤더에서 RefreshToken 추출 - 없거나 유효하지 않으면 null 반환
-        String refreshToken = jwtService.extractRefreshToken(httpServletRequest)
-                .filter(jwtService::isTokenValid)
-                .orElse(null);
+        if(httpServletRequest.getRequestURI().equals("/api/refresh")) {
+            // 요청 헤더에서 RefreshToken 추출 - 없거나 유효하지 않으면 null 반환
+            String refreshToken = jwtService.extractRefreshToken(httpServletRequest)
+                    .filter(jwtService::isTokenValid)
+                    .orElse(null);
+            // 요청 헤더에 RefreshToken이 존재한다면
+            if(refreshToken != null)
+                // 헤더의 RefreshToken과 Redis의 RefreshToken 비교 => 일치한다면 AccessToken 재발급
+                checkRefreshTokenAndReIssueAccessToken(httpServletResponse, refreshToken);
+        }
 
-        // 요청 헤더에 RefreshToken이 존재한다면
-        if(refreshToken != null)
-            // 헤더의 RefreshToken과 Redis의 RefreshToken 비교 => 일치한다면 AccessToken 재발급
-            checkRefreshTokenAndReIssueAccessToken(httpServletResponse, refreshToken);
-            // 요청 헤더에 RefreshToken이 존재하지 않는다면
         else
             // AccessToken 검사 및 인증 처리
             // AccessToken이 존재하지 않거나 유효하지 않다면 => 인증 객체가 담기지 않은 상태로 인증 실패(403)
