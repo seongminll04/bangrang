015809c146ad6dc2c754ddb87ee50ecd1d0ed16b
이성민 <lrune2el@naver.com>
2023-11-03 09:26:34 +0900

    fix : env 변수명 오류

--- FrontEnd_Web/src/pages/login.tsx
+++ FrontEnd_Web/src/pages/login.tsx
@@ -1,6 +1,10 @@
-import React, { useEffect } from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import { useNavigate, Link } from 'react-router-dom';
 const Login: React.FC = () => {
+  const [isId, setId] = useState('')
+  const [isPw, setPw] = useState('')
+
     const navigate = useNavigate()
     useEffect(()=>{
       const AccessToken = localStorage.getItem('AccessToken')
@@ -8,16 +12,48 @@ const Login: React.FC = () => {
         navigate('/manage')
       }
     },[navigate])
+
+  const login = (e:any) => {
+    e.preventDefault();
+    if (!isId) {
+      if (!isPw) {
+        return alert('아이디와 비밀번호를 입력해주세요.')
+      }
+      return alert('아이디를 입력해주세요.')
+    }
+    if (!isPw) return alert('패스워드를 입력해주세요.')
+    axios({
+      method:'post',
+      url:`${process.env.REACT_APP_API}/web/login`,
+      data:{
+        id:isId,
+        password:isPw
+      }
+    }).then(res=>{
+      localStorage.setItem("AccessToken",res.headers['authorization']);
+      localStorage.setItem('RefreshToken',res.headers['authorization-refresh']);
+      const UserName = res.data.organizationName
+      localStorage.setItem("UserName",UserName)
+      if (UserName === 'admin@bangrang') {
+        navigate('/admin')
+      } else {
+        navigate('/manage')
+      }
+    }).catch(err => {
+      alert('해당 계정이 존재하지않습니다.\n아이디 & 비밀번호를 다시 확인해주세요')
+    })
+    
+  }
   return (
     <div>
         <img src="assets/logo.png" alt="" />
         <h1>관리자 로그인</h1>
-        <form onSubmit={()=>navigate('/manage')}>
+        <form onSubmit={login}>
             <label>아이디</label>
-            <input type="email" name="" id="" />
+            <input type="text" value={isId} onChange={(e)=>{setId(e.target.value)}} />
             <br />
             <label>비밀번호</label>
-            <input type="password" name="" id="" />
+            <input type="password" value={isPw} onChange={(e)=>{setPw(e.target.value)}} />
             <br />
             <button type="submit">로그인</button>
         </form>
