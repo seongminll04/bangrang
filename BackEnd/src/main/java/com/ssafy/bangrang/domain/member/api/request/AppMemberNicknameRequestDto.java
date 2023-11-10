ce714b6d952c2ce8df6dbfe40bce250eeba34cb1
이성민 <lrune2el@naver.com>
2023-11-10 16:10:42 +0900

    fix : nicknameplus builder error#2

--- BackEnd/src/main/java/com/ssafy/bangrang/domain/member/api/request/AppMemberNicknameRequestDto.java
+++ BackEnd/src/main/java/com/ssafy/bangrang/domain/member/api/request/AppMemberNicknameRequestDto.java
@@ -2,8 +2,11 @@ package com.ssafy.bangrang.domain.member.api.request;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class AppMemberNicknameRequestDto {
     @NotBlank(message = "nickname은 빈값이 올 수 없습니다")
     private String nickname;
