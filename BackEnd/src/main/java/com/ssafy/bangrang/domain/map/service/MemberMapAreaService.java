d4f14cb4714858cef45ed63f24862c9888d33f57
aldms8960 <45choi22@gmail.com>
2023-11-14 16:55:11 +0900

    Fix: datasource 변경

--- BackEnd/src/main/java/com/ssafy/bangrang/domain/map/service/MemberMapAreaService.java
+++ BackEnd/src/main/java/com/ssafy/bangrang/domain/map/service/MemberMapAreaService.java
@@ -1,6 +1,7 @@
 package com.ssafy.bangrang.domain.map.service;
 
 import com.ssafy.bangrang.domain.map.api.request.AddMarkersRequestDto;
+import com.ssafy.bangrang.domain.map.api.response.GeometryBorderCoordinate;
 import org.locationtech.jts.geom.Point;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,5 +11,5 @@ import java.util.List;
 public interface MemberMapAreaService {
 
     @Transactional
-    List<List<Point>> addMeberMapArea(UserDetails userDetails, List<AddMarkersRequestDto> addMarkersRequestDtoList);
+    List<List<GeometryBorderCoordinate>> addMeberMapArea(UserDetails userDetails, List<AddMarkersRequestDto> addMarkersRequestDtoList);
 }
